88cae9336ba9e8497238ed47ce63ece2e6e5e356
Adrian Mysliwiec <85237040+MysliwiecAdrian@users.noreply.github.com>
2026-04-11 14:06:02 -0400

    Add: permutations - submission-0

---
+++ Data Structures & Algorithms/permutations/submission-0.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<vector<int>> res;
+    
+    vector<vector<int>> permute(vector<int>& nums) {
+        backtrack(nums, 0);
+        return res;
+    }
+
+    void backtrack(vector<int>& nums, int i){
+        if (i >= nums.size()){
+            res.push_back(nums);
+            return;
+        }
+
+        for(int index = i; index < nums.size(); index++){
+            swap(nums[i], nums[index]);
+            backtrack(nums, i + 1);
+            swap(nums[i], nums[index]);
+        }
+    }
+};

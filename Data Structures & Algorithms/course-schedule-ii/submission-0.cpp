71c2b1f4d4853355be4563c32b0866ce4a332560
Adrian Mysliwiec <85237040+MysliwiecAdrian@users.noreply.github.com>
2026-04-22 11:58:51 -0400

    Add: course-schedule-ii - submission-0

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-0.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<int> res;
+    vector<vector<int>> adj;
+    vector<int> pq;
+    
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        adj = vector<vector<int> >(numCourses);
+        pq = vector<int>(numCourses, 0);
+
+        for (auto& prereq : prerequisites){
+            pq[prereq[0]]++;
+            adj[prereq[1]].push_back(prereq[0]);
+        }
+
+        for (int i = 0; i < numCourses; i++){
+            if (pq[i] == 0){
+                dfs(i);
+            }
+        }
+
+        if (res.size() != numCourses){ return {};}
+        return res;
+    }
+
+    void dfs(int c){
+        res.push_back(c);
+        pq[c]--;
+        for (int n : adj[c]){
+            pq[n]--;
+            if (pq[n] == 0){
+                dfs(n);
+            }
+        }
+    }
+};
